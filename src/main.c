4b2effb6fbe0e61c5b40dce914095b6a5c4774b6
luiscoast <lacc2@cesar.school>
2024-05-15 14:40:34 -0300

    Inimigos;personagens;movimentos
    Nessa versão, baseado no main.c de exemplo, criei a lista encadeada de inimigos, o struct do personagem e a função que vai reger o movimento dos inimigos

--- src/main.c
+++ src/main.c
@@ -1,88 +1,114 @@
-/**
- * main.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
-
 #include <string.h>
 
-#include "screen.h"
 #include "keyboard.h"
+#include "screen.h"
 #include "timer.h"
 
-int x = 34, y = 12;
-int incX = 1, incY = 1;
+#define MAX_CORPO_LENGTH 12
 
-void printHello(int nextX, int nextY)
-{
-    screenSetColor(CYAN, DARKGRAY);
-    screenGotoxy(x, y);
-    printf("           ");
-    x = nextX;
-    y = nextY;
-    screenGotoxy(x, y);
-    printf("Hello World");
+struct Inimigo {
+  int x_inimigo;
+  int y_inimigo;
+  char corpo[MAX_CORPO_LENGTH];
+  struct Inimigo *proximo;
+};
+
+struct Personagem {
+  int x_personagem;
+  int y_personagem;
+  char corpo[MAX_CORPO_LENGTH];
+};
+
+void print_inimigo(struct Inimigo *inimigo, struct Personagem *personagem) {
+  screenSetColor(CYAN, DARKGRAY);
+  screenGotoxy(x, y);
+  printf("           ");
+  x = nextX;
+  y = nextY;
+  screenGotoxy(x, y);
+  printf("Hello World");
 }
 
-void printKey(int ch)
-{
-    screenSetColor(YELLOW, DARKGRAY);
-    screenGotoxy(35, 22);
-    printf("Key code :");
+void print_personagem(struct Inimigo *inimigo, struct Personagem *personagem) {
+  screenSetColor(CYAN, DARKGRAY);
+  screenGotoxy(x, y);
+  printf("           ");
+  x = nextX;
+  y = nextY;
+  screenGotoxy(x, y);
+  printf("Hello World");
+}
 
-    screenGotoxy(34, 23);
-    printf("            ");
+void newloc(struct Inimigo *inimigo, struct Personagem *personagem){
+    int x_inimigo_proximo, y_inimigo_proximo;
+    int x_inimigo_atual = inimigo -> x_inimigo;
+    int y_inimigo_atual = inimigo -> y_inimigo;
+    int x_personagem_atual = personagem -> x_personagem;
+    int y_personagem_atual = personagem -> y_personagem;
+
+    if (x_inimigo_atual > x_personagem_atual){
+        x_inimigo_proximo = x_inimigo_atual - 1;
+    }else if (x_inimigo_atual < x_personagem_atual){
+        x_inimigo_proximo = x_inimigo_atual + 1;
+    }else if(x_inimigo_atual == x_personagem_atual){
+        if (y_inimigo_atual > y_personagem_atual){
+            y_inimigo_proximo = y_inimigo_atual - 1;
+        }else if (x_inimigo_atual < x_personagem_atual){
+            y_inimigo_proximo = y_inimigo_atual + 1;
+        }
+    }        
     
-    if (ch == 27) screenGotoxy(36, 23);
-    else screenGotoxy(39, 23);
-
-    printf("%d ", ch);
-    while (keyhit())
-    {
-        printf("%d ", readch());
+    
+    if (y_inimigo_atual > y_personagem_atual){
+        y_inimigo_proximo = y_inimigo_atual - 1;
+    }else if (y_inimigo_atual < y_personagem_atual){
+        y_inimigo_proximo = y_inimigo_atual + 1;
+    }else if (y_inimigo_atual == y_personagem_atual){
+        if (x_inimigo_atual > x_personagem_atual){
+            x_inimigo_proximo = x_inimigo_atual - 1;
+        }else if (x_inimigo_atual < x_personagem_atual){
+            x_inimigo_proximo = x_inimigo_atual + 1;
+        }
     }
 }
 
-int main() 
-{
-    static int ch = 0;
+int main() {
+  static int ch = 0;
 
-    screenInit(1);
-    keyboardInit();
-    timerInit(50);
+  screenInit(1);
+  keyboardInit();
+  timerInit(50);
 
-    printHello(x, y);
-    screenUpdate();
+  print_inimigo(x, y);
+  screenUpdate();
 
-    while (ch != 10) //enter
-    {
-        // Handle user input
-        if (keyhit()) 
-        {
-            ch = readch();
-            printKey(ch);
-            screenUpdate();
-        }
-
-        // Update game state (move elements, verify collision, etc)
-        if (timerTimeOver() == 1)
-        {
-            int newX = x + incX;
-            if (newX >= (MAXX -strlen("Hello World") -1) || newX <= MINX+1) incX = -incX;
-            int newY = y + incY;
-            if (newY >= MAXY-1 || newY <= MINY+1) incY = -incY;
-
-            printKey(ch);
-            printHello(newX, newY);
-
-            screenUpdate();
-        }
+  while (ch != 10) // enter
+  {
+    // Handle user input
+    if (keyhit()) {
+      ch = readch();
+      printKey(ch);
+      screenUpdate();
     }
 
-    keyboardDestroy();
-    screenDestroy();
-    timerDestroy();
+    if (timerTimeOver() == 1) {
+      int newX = x + incX;
+      if (newX >= (MAXX - strlen("Hello World") - 1) || newX <= MINX + 1)
+        incX = -incX;
+      int newY = y + incY;
+      if (newY >= MAXY - 1 || newY <= MINY + 1)
+        incY = -incY;
 
-    return 0;
+      printKey(ch);
+      print_inimigo(newX, newY);
+
+      screenUpdate();
+    }
+  }
+
+  keyboardDestroy();
+  screenDestroy();
+  timerDestroy();
+
+  return 0;
 }
